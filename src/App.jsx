b40846300b8b9794f46cbea5b0971e270d157fd4
anickacodes <anickalewis@pursuit.org>
2024-12-21 12:53:01 -0500

    chore: visually appeasing updates

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/clnStylez" element={<NHS />} />
+        <Route path="/nhs" element={<NHS />} />
         <Route path="/affiliates" element={<Contact />} />
       </Routes>
       <Footer/>
